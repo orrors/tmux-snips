ba3b27344d81daf96d4c8b88403c06013b691a42
Connor B <80982854+connbrack@users.noreply.github.com>
2026-04-13 13:33:39 -0400

    Add editor check

--- scripts/snips.sh
+++ scripts/snips.sh
@@ -13,6 +13,13 @@ elif hash batcat 2>/dev/null ; then
 fi
 # }}}
 
+# set editor {{{
+editor="vim"
+if hash nvim 2>/dev/null ; then
+    editor="nvim"
+fi
+# }}}
+
 selection=$(rg --line-number --no-heading --color=always "^\s*##+ " . |
   fzf --ansi \
       --layout=reverse \
@@ -20,13 +27,13 @@ selection=$(rg --line-number --no-heading --color=always "^\s*##+ " . |
       --preview "awk -v start={2} \"NR >= start { if (NR > start && \\\$0 ~ /^#+ /) exit; print }\" {1} | ${previewer}" \
       --preview-window=right:60%:wrap \
       --header=$'\033[1;33mctrl-e\033[m: edit file | \033[1;33mctrl-y\033[m: edit before copy' \
-      --bind 'ctrl-e:execute(vim +{2} {1})+abort' \
+      --bind 'ctrl-e:execute(${editor} +{2} {1})+abort' \
       --bind 'ctrl-y:execute(echo NOT IMPLEMENTED)+abort')
 
 if [ -n "$selection" ] ; then
-    local FILE=$(echo "$selection" | cut -d: -f1)
-    local LINE=$(echo "$selection" | cut -d: -f2)
-    local TMP_FILE=$(mktemp)
+    FILE=$(echo "$selection" | cut -d: -f1)
+    LINE=$(echo "$selection" | cut -d: -f2)
+    TMP_FILE=$(mktemp)
     awk -v start="$LINE" '
         NR < start { next }
         NR > start && /^#+ / { exit }
@@ -37,7 +44,7 @@ if [ -n "$selection" ] ; then
         }
         in_block { print }
     ' "$FILE" > "$TMP_FILE"
-    vim "$TMP_FILE"
+    $editor "$TMP_FILE"
     cat "$TMP_FILE" | tmux load-buffer -w -
     rm "$TMP_FILE"
 fi
