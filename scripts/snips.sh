04c48f32f2b4ce9eb4c9213a19fbfc1f7fafcc67
eximus <fabio4335@gmail.com>
2026-04-12 23:27:38 -0400

    add clipboard on selection

--- scripts/snips.sh
+++ scripts/snips.sh
@@ -1,22 +1,49 @@
 #!/usr/bin/env bash
+# vi: foldmethod=marker
 
 CURRENT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 cd "$CURRENT_DIR/../snips/"
 
+# set previewer {{{
 previewer="cat"
 if hash bat 2>/dev/null ; then
     previewer="bat -fpl markdown"
 elif hash batcat 2>/dev/null ; then
     previewer="batcat -fpl markdown"
 fi
+# }}}
+# set copy command {{{
+clipboard=
+if hash wl-copy 2>/dev/null ; then
+    clipboard=wl-copy
+elif hash xclip 2>/dev/null ; then
+    clipboard='xclip -r -in -selection clipboard'
+else
+    echo Theres no clipboard available!
+    exit 1
+fi
+# }}}
 
-rg --line-number --no-heading --color=always "^\s*#+ " . |
+selection=$(rg --line-number --no-heading --color=always "^\s*#+ " . |
   fzf --ansi \
       --layout=reverse \
       --delimiter : \
       --preview "awk -v start={2} \"NR >= start { if (NR > start && \\\$0 ~ /^#+ /) exit; print }\" {1} | ${previewer}" \
       --preview-window=right:60%:wrap \
       --header=$'\033[1;33mctrl-e\033[m: edit file' \
-      --bind 'ctrl-e:execute(vim +{2} {1})+abort' |
-awk -F: '{print "+"$2" "$1}' |
-xargs -r vim
+      --bind 'ctrl-e:execute(vim +{2} {1})+abort' )
+
+if [ -n "$selection" ] ; then
+    FILE=$(echo "$selection" | cut -d: -f1)
+    LINE=$(echo "$selection" | cut -d: -f2)
+    awk -v start="$LINE" '
+        NR < start { next }
+        NR > start && /^#+ / { exit }
+        /^```/ {
+            in_block = !in_block;
+            if (!in_block) exit;
+            next
+        }
+        in_block { print }
+    ' "$FILE" | $clipboard
+fi
