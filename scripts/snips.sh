97782e07440cc2b25a389fdcf812419a6dafcd9d
Connor B <80982854+connbrack@users.noreply.github.com>
2026-04-24 12:28:04 -0400

    Feature: use custom dir, add view in buffer option

--- scripts/snips.sh
+++ scripts/snips.sh
@@ -2,7 +2,8 @@
 # vi: foldmethod=marker
 
 CURRENT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
-cd "$CURRENT_DIR/../snips/"
+DEFAULT_DIR="$CURRENT_DIR/../snips"
+CUSTOM_DIR=$1
 
 # set previewer {{{
 previewer="cat"
@@ -42,15 +43,32 @@ edit_before_copy() {
 export -f edit_before_copy
 # }}}
 
-selection=$(rg --line-number --no-heading --color=always "^\s*##+ " . | sed $'s/.*\/\([^./]\+\).md/\033[1;35m\\1\033[m:&/;s/##\+//' |
+# open in buffer function {{{
+open_in_buffer() {
+    FILE="$1"
+    LINE="$2"
+    TMP_FILE=$(mktemp)
+    awk -v start="$LINE" '
+        NR < start { next }
+        NR > start && /^#+ / { exit }
+        { print }' "$FILE" > "$TMP_FILE"
+    $editor "$TMP_FILE"
+    cat "$TMP_FILE" | tmux load-buffer -w -
+    rm "$TMP_FILE"
+}
+export -f open_in_buffer 
+# }}}
+
+selection=$(rg --line-number --no-heading --color=always "^\s*##+ " $DEFAULT_DIR $CUSTOM_DIR | sed $'s/.*\/\([^./]\+\).md/\033[1;35m\\1\033[m:&/;s/##\+//' |
   fzf --ansi \
       --layout=reverse \
       --with-nth=1,4.. \
       --delimiter : \
       --preview "awk -v start={3} \"NR >= start { if (NR > start && \\\$0 ~ /^#+ /) exit; print }\" {2} | ${previewer}" \
       --preview-window=right:60%:wrap \
-      --header=$'\033[1;33mctrl-e\033[m: edit file | \033[1;33mctrl-y\033[m: edit before copy' \
+      --header=$'\033[1;33mctrl-b\033[m: open in buffer | \033[1;33mctrl-y\033[m: edit before copy | \033[1;33mctrl-e\033[m: edit file'  \
       --bind 'ctrl-e:execute(${editor} -R +{3} {2})+abort' \
+      --bind 'ctrl-b:execute(open_in_buffer {2} {3})+abort' \
       --bind 'ctrl-y:execute(edit_before_copy {2} {3})+abort')
 
 if [ -n "$selection" ] ; then
